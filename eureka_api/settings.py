5b2a867a836ad006da266e29244c8df5e3c118dd
FelipeCarillo <felipecarillo@outlook.com>
2024-07-23 21:14:07 -0300

    Update database configuration in settings.py

--- eureka_api/settings.py
+++ eureka_api/settings.py
@@ -81,34 +81,27 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'eureka_api.wsgi.application'
-#
-#
+
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Postgres2023!',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
+NAME = os.getenv('DB_DB')
+USER = os.getenv('DB_USER')
+PASSWORD = os.getenv('DB_PASSWORD')
+HOST = os.getenv('DB_HOST')
+PORT = os.getenv('DB_PORT')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': PORT,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
