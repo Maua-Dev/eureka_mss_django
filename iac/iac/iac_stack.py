8aa0706146f883a4d866974e96d004e59778d3f7
FelipeCarillo <felipecarillo@outlook.com>
2024-07-23 22:39:43 -0300

    refactor(iac): Update FargateStack to use a different container name and pull image from ECR repository

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -23,15 +23,15 @@ class IacStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        self.aws_account_id = os.environ.get("AWS_ACCOUNT_ID")
-        self.github_ref_name = os.environ.get("GITHUB_REF_NAME")
+        self.STAGE = os.environ.get("STAGE")
         self.project_name = os.environ.get("PROJECT_NAME")
+        self.respository_name = os.environ.get("REPOSITORY_NAME")
 
-        REMOVAL_POLICY = RemovalPolicy.RETAIN if 'prod' in self.github_ref_name else RemovalPolicy.DESTROY
+        REMOVAL_POLICY = RemovalPolicy.RETAIN if 'prod' in self.STAGE else RemovalPolicy.DESTROY
 
         self.network_stack = NetworkStack(self, "EurekaNetworkStack")
 
         self.rds_stack = RDSStack(self, self.network_stack.vpc)
 
-        # self.fargate_stack = FargateStack(self, "EurekaFargateStack", rds_instance=self.rds_stack.rds, vpc=self.network_stack.vpc,
-        #                                   ecs_cluster=self.network_stack.ecs_cluster)
+        self.fargate_stack = FargateStack(self, "EurekaFargateStack", rds_instance=self.rds_stack.rds, vpc=self.network_stack.vpc,
+                                          ecs_cluster=self.network_stack.ecs_cluster, repository_name=self.respository_name)
