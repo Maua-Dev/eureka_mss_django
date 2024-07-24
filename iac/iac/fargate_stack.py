3a25ae565a2496b56d914b34ee233fd7ea822c6a
FelipeCarillo <felipecarillo@outlook.com>
2024-07-24 16:22:05 -0300

    refactor(iac): Update FargateStack to use a different container name and pull image from ECR repository

--- iac/iac/fargate_stack.py
+++ iac/iac/fargate_stack.py
@@ -20,13 +20,12 @@ class FargateStack(Construct):
         construct_id: str,
         vpc: ec2.Vpc,
         ecs_cluster: ecs.Cluster,
-        # rds_instance: rds.DatabaseInstance,
+        rds_instance: rds.DatabaseInstance,
         task_cpu: int = 256,
         task_memory_mib: int = 1024,
         task_desired_count: int = 1,
         task_min_scaling_capacity: int = 2,
         task_max_scaling_capacity: int = 4,
-        repository_name: str = None,
         **kwargs,
     ) -> None:
         super().__init__(scope, construct_id)
@@ -40,8 +39,6 @@ class FargateStack(Construct):
 
         self.container_name = f"eureka_django_app"
 
-        repository = ecr.Repository.from_repository_name(self, "EurekaDjangoAppRepo", repository_name=repository_name)
-
         # Create the load balancer, ECS service and fargate task for teh Django App
         self.alb_fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
             self,
@@ -53,17 +50,20 @@ class FargateStack(Construct):
             memory_limit_mib=self.task_memory_mib,  # Default is 512
             desired_count=self.task_desired_count,  # Default is 1
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_ecr_repository(repository, tag="latest"),
+                image=ecs.ContainerImage.from_asset(
+                    directory="../",
+                    file="Dockerfile",
+                ),
                 container_name=self.container_name,
                 container_port=8000,
                 environment={
                     "STAGE": "DEV",
                     "DJANGO_SETTINGS_MODULE": "eureka_api.settings",
-                    # "DB_NAME": rds_instance.instance_identifier,
-                    # "DB_USER": rds_instance.secret.secret_value_from_json("username").unsafe_unwrap(),
-                    # "DB_PASSWORD": rds_instance.secret.secret_value_from_json("password").unsafe_unwrap(),
-                    # "DB_HOST": rds_instance.db_instance_endpoint_address,
-                    # "DB_PORT": rds_instance.db_instance_endpoint_port,
+                    "DB_NAME": rds_instance.instance_identifier,
+                    "DB_USER": rds_instance.secret.secret_value_from_json("username").unsafe_unwrap(),
+                    "DB_PASSWORD": rds_instance.secret.secret_value_from_json("password").unsafe_unwrap(),
+                    "DB_HOST": rds_instance.db_instance_endpoint_address,
+                    "DB_PORT": rds_instance.db_instance_endpoint_port,
                 },
             ),
             public_load_balancer=True,
