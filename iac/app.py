562618b69ce3f5ee9238e05572833e2d20b6a3f3
FelipeCarillo <felipecarillo@outlook.com>
2024-08-01 09:59:04 -0300

    refactor(iac): Remove unused environment variable and update AWS configuration

--- iac/app.py
+++ iac/app.py
@@ -15,7 +15,6 @@ app = cdk.App()
 aws_region = os.environ.get("AWS_REGION")
 aws_account_id = os.environ.get("AWS_ACCOUNT_ID")
 stack_name = os.environ.get("STACK_NAME")
-project_name = os.environ.get("PROJECT_NAME")
 
 github_ref_name = os.environ.get("STAGE")
 
