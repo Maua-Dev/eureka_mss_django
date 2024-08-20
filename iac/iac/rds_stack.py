00bccfdb55f45b1bf04b215085fe04e44f27da8b
FelipeCarillo <felipecarillo@outlook.com>
2024-08-19 23:23:22 -0300

    Refactor RDS stack to use Aurora PostgreSQL version 15.6

--- iac/iac/rds_stack.py
+++ iac/iac/rds_stack.py
@@ -12,25 +12,29 @@ class RDSStack(Construct):
     def __init__(self, scope: Construct, vpc: ec2.Vpc, database_name: str) -> None:
         super().__init__(scope, "EurekaRDSStack")
 
-        self.rds = rds.DatabaseInstance(
-            self,
-            "EurekaRDS",
+        self.rds = rds.DatabaseCluster(
+            self, "EurekaRDS",
             engine=rds.DatabaseClusterEngine.aurora_postgres(
                 version=rds.AuroraPostgresEngineVersion.VER_15_6
             ),
-            instance_type=ec2.InstanceType.of(
-                ec2.InstanceClass.T3, ec2.InstanceSize.MICRO
-            ),
-            vpc=vpc,
+            instance_props={
+                "instance_type": ec2.InstanceType.of(
+                    ec2.InstanceClass.T3, ec2.InstanceSize.MICRO
+                ),
+                "vpc_subnets": {
+                    "subnet_type": ec2.SubnetType.PRIVATE
+                },
+                "vpc": vpc,
+                "security_group": ec2.SecurityGroup(
+                    self, "EurekaRDSSecurityGroup",
+                    vpc=vpc,
+                    allow_all_outbound=True
+                )
+            },
+            instances=1,
+            default_database_name=database_name,
+            removal_policy=RemovalPolicy,
             credentials=rds.Credentials.from_generated_secret("postgres"),
-            removal_policy=RemovalPolicy.DESTROY,
-            database_name=database_name,
-            storage_type=rds.StorageType.GP3,
-            allocated_storage=20,
-            max_allocated_storage=100,
-            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-            publicly_accessible=True,
         )
-
+        
         self.rds.connections.allow_default_port_from_any_ipv4()
-
