19d834d8160b93e89d75bcc7e6064ec91d1ccff8
FelipeCarillo <felipecarillo@outlook.com>
2024-08-20 22:41:10 -0300

    Refactor Fargate stack to use RDS cluster and update environment variables

--- iac/iac/rds_stack.py
+++ iac/iac/rds_stack.py
@@ -11,21 +11,25 @@ class RDSStack(Construct):
     def __init__(self, scope: Construct, vpc: ec2.Vpc, database_name: str) -> None:
         super().__init__(scope, "EurekaRDSStack")
 
-        self.rds = rds.ServerlessCluster(
-            self, "EurekaRDS",
+        self.rds = rds.DatabaseCluster(
+            self,
+            "EurekaRDS",
             engine=rds.DatabaseClusterEngine.aurora_postgres(
                 version=rds.AuroraPostgresEngineVersion.VER_15_6
             ),
-            vpc=vpc,
             default_database_name=database_name,
             removal_policy=RemovalPolicy.DESTROY,
-            credentials=rds.Credentials.from_generated_secret("postgres"),
-            scaling=rds.ServerlessScalingOptions(
-                auto_pause=cdk.Duration.minutes(5),
-                min_capacity=rds.AuroraCapacityUnit.ACU_1,
-                max_capacity=rds.AuroraCapacityUnit.ACU_2,
+            instance_props=rds.InstanceProps(
+                instance_type=ec2.InstanceType.of(
+                    ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL
+                ),
+                vpc_subnets= rds.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+                vpc=vpc,
             ),
-            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),   
+            instances=1,
+            serverless_v2_max_capacity=1,
+            serverless_v2_min_capacity=0.5,
+            credentials=rds.Credentials.from_generated_secret("postgres"),
         )
         
         self.rds.connections.allow_default_port_from_any_ipv4()
